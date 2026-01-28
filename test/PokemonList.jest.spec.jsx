1045c774a7c76b304349fe40ef65c63b461ab711
jukaveka <juhokavekari@hotmail.com>
2026-01-28 19:43:41 +0200

    Fix broken test #skip

--- test/PokemonList.jest.spec.jsx
+++ test/PokemonList.jest.spec.jsx
@@ -22,7 +22,7 @@ describe('<PokemonList />', () => {
         <PokemonList pokemonList={pokemonList} />
       </BrowserRouter>
     )
-    expect(screen.getByText('bulbasauur')).toBeVisible()
+    expect(screen.getByText('bulbasaur')).toBeVisible()
     expect(screen.getByText('eevee')).toBeVisible()
   })
 })
